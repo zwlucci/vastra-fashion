a4e29ba81774d257e6dcfae42dc144f0cbfb46f1
zwlucci <298680123+zwlucci@users.noreply.github.com>
2026-07-07 21:25:00 +0545

    Support Ethernet-to-Wi-Fi development

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import http from "node:http";
+import { networkInterfaces } from "node:os";
 import { app, corsOrigin } from "./app.js";
 import { initSocket } from "./socket.js";
 
@@ -16,5 +17,13 @@ const server = http.createServer(app);
 initSocket(server, corsOrigin);
 
 server.listen(port, host, () => {
-  console.log(`VASTRA API listening on http://${host}:${port}`);
+  const networkAddresses = Object.values(networkInterfaces())
+    .flat()
+    .filter((address) => address?.family === "IPv4" && !address.internal)
+    .map((address) => address.address);
+
+  console.log(`Backend local:   http://localhost:${port}`);
+  networkAddresses.forEach((address) => {
+    console.log(`Backend network: http://${address}:${port}`);
+  });
 });
